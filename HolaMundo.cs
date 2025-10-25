81f97b98e0fb1bbbc2fa7eb08e9d8ad90de64799
ProfAngel007 <angelikuzt@gmail.com>
2025-10-25 00:03:53 -0300

    Add files via upload
    e

---
+++ HolaMundo.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HolaMundo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Imprime el texto "Hola mundo" en la ventana de comandos
+        print("Hola mundo");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
